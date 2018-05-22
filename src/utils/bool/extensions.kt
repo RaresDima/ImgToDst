220d578e516ffc05890151a40c3f629fd085f066
Rares Dima <rares.dima96@yahoo.com>
2018-05-22 14:38:55 +0300

    Added maven support.

--- src/utils/bool/extensions.kt
+++
@@ -1,9 +0,0 @@
-package utils.bool
-
-
-/**
- * Returns 0 if the current [Boolean] is false, 1 if it is true.
- *
- * @return 0 if the current [Boolean] is false, 1 if it is true.
- */
-fun Boolean.toInt() : Int = if (this) 1 else 0
